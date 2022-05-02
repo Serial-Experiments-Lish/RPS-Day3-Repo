13db553b674f3f4996b88e735369e033253c4ccd
ShengtraneChooChoo <lishengashiluli@gmail.com>
2022-05-02 07:08:03 -0400

    Clean coding

--- rps.js
+++ rps.js
@@ -1,21 +1,29 @@
-
+//Trying my hand at clean code.
+//Variable that contains every hand for a proper rps showdown.
 const hands = ['rock', 'paper', 'scissors'];
 
-function getHands() {
+//Changed function into an arrow function
+//function getHands() {
+//Function that returns a random hand from the hands variable.
+getHands = () => {
     let randomDraw = parseInt(Math.random()*10)%3;
     return hands[randomDraw];
 }
 
+//My valiant players.
 let playerOne = {name: 'Johnny Bravo', getHands: function () {return getHands();}};
 let playerTwo = {name: 'Lafayette', getHands: function () {return getHands();}};
 
+//Variable that determines which hand beats which.
 let winRequirement = {
     rock: 'scissors',
     paper: 'rock',
     scissors: 'paper'
 }
 
-function playRound(p1, p2) {
+//Changed the function into an arrow function for straight-to-the-point functioning.
+//function playRound(p1, p2) {
+playRound = (p1, p2) => {
     const pOneHand = p1.getHands();
     const pTwoHand = p2.getHands();
 
@@ -34,6 +42,7 @@ function playRound(p1, p2) {
     }
 }
 
+//Function that allows the game to run.
 let playGame = (playerUno, playerDos, playUntil) => {
         let playerUnoW = 0;
         let playerDosW = 0;
@@ -58,7 +67,9 @@ let playGame = (playerUno, playerDos, playUntil) => {
 
 console.log(playGame(playerOne, playerTwo, 5));
 
-function getHand (callback, hand) {
+//Changed the arrow function
+//function getHand (callback, hand) {
+getHand = (callback, hand) => {
     if (hand !== "scissors") {
         callback(hand);
     }
